c85ffc5efa64e55417a208e2c6c598c5b43538e7
Ihar Saroka <Ihar_Saroka@epam.com>
2018-12-04 12:36:27 +0300

    changed prefix for components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'learn-portal-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
