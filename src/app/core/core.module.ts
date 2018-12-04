aabee92725d817e80f0266dd3c48953c69140302
Ihar Saroka <Ihar_Saroka@epam.com>
2018-12-04 15:20:24 +0300

    task-1-intro: generated header, footer and breadcrumbs components in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent]
 })
 export class CoreModule { }
