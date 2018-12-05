2e5b3912e7a8f81d6c27445b276dd0fc0237ff37
Ihar Saroka <Ihar_Saroka@epam.com>
2018-12-05 12:02:32 +0300

    task-1-intro: added courses page component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CoursesComponent]
 })
 export class PagesModule { }
