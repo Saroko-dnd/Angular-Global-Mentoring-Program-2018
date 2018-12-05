6a02c923b1a4374d675c5c0c2e16ada10cfad0f7
Ihar Saroka <Ihar_Saroka@epam.com>
2018-12-05 17:40:59 +0300

    task-1-intro: component breadcrumbs was moved inside header component folder

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { BreadcrumbsComponent } from './header/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent],
-  exports: [HeaderComponent, FooterComponent, BreadcrumbsComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
