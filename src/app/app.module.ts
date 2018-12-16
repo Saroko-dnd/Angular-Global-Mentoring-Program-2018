b49c2ec18f36f75394ef3032ba5add937ca85840
saroko-dnd <isaroko@mail.ru>
2018-12-16 02:36:30 +0200

    task-1-intro: added prettier-tslint package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { CoreModule } from "src/app/core";
-import { CoursesModule } from "src/app/courses";
-import { LoginModule } from "./login";
+import { CoreModule } from 'src/app/core';
+import { CoursesModule } from 'src/app/courses';
+import { LoginModule } from './login';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
-import { appRoutes } from "./app.routing";
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [AppComponent],
