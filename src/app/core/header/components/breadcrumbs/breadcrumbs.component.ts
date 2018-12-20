ab0ee78dd5202c3d554c5b1e1d0ca4aaad1f4f75
Ihar Saroka <Ihar_Saroka@epam.com>
2018-12-20 10:11:34 +0300

    task-2-components: sass refactoring

--- src/app/core/header/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/core/header/components/breadcrumbs/breadcrumbs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'learn-portal-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.css']
+  styleUrls: ['./breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent implements OnInit {
 
