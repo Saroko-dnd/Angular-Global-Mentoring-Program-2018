8291e11cc2d124abfcd9ede0a2b83e5c3f8879b9
saroko-dnd <isaroko@mail.ru>
2018-12-23 20:16:05 +0200

    task-2-components: all remaining .css files were changed to .scss

--- src/app/courses/components/create-new-course/create-new-course.component.ts
+++ src/app/courses/components/create-new-course/create-new-course.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'learn-portal-create-new-course',
   templateUrl: './create-new-course.component.html',
-  styleUrls: ['./create-new-course.component.css']
+  styleUrls: ['./create-new-course.component.scss']
 })
 export class CreateNewCourseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
