f1358a58162e415713563aad57cbed90aa969132
saroko-dnd <isaroko@mail.ru>
2018-12-30 03:32:51 +0200

    task-3-unit-testing: created proper spec for Courses service

--- src/app/courses/services/courses/courses.service.spec.ts
+++ src/app/courses/services/courses/courses.service.spec.ts
@@ -3,14 +3,18 @@ import { TestBed } from '@angular/core/testing';
 import { CoursesService } from './courses.service';
 
 describe('CoursesService', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      providers: [CoursesService]
-    })
-  );
+  describe('getListOfCourses()', () => {
+    beforeEach(() =>
+      TestBed.configureTestingModule({
+        providers: [CoursesService]
+      })
+    );
 
-  it('should be created', () => {
-    const service: CoursesService = TestBed.get(CoursesService);
-    expect(service).toBeTruthy();
+    it('should return not empty array of courses', () => {
+      const service: CoursesService = TestBed.get(CoursesService);
+      const courses = service.getListOfCourses();
+
+      expect(courses.length > 0).toBe(true);
+    });
   });
 });
