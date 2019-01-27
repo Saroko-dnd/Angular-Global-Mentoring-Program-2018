e110c09699fc8696731828773d74bbc74d0035fe
saroko-dnd <isaroko@mail.ru>
2019-01-27 16:44:45 +0200

    task-5-modules-and-services: created method createCourse for courses service

--- src/app/courses/services/courses/courses.service.ts
+++ src/app/courses/services/courses/courses.service.ts
@@ -2,89 +2,90 @@ import { Injectable } from '@angular/core';
 
 import { ICourse } from 'src/app/shared';
 
+import Guid from 'guid';
+
 @Injectable()
 export class CoursesService {
+  static courses: ICourse[] = [
+    {
+      creationDate: new Date(Date.now() - 172800000),
+      description: `Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
+      est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
+      amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
+      laborum atque aut id necessitatibus asperiores minus debitis fugit amet
+      dolorum assumenda nam obcaecati vitae in sequi!`,
+      duration: 120,
+      id: '1',
+      title: 'JavaScript: Understanding the Weird Parts',
+      topRated: false
+    },
+    {
+      creationDate: new Date(Date.now() - 86400000),
+      description: `Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
+      est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
+      amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
+      laborum atque aut id necessitatibus asperiores minus debitis fugit amet
+      dolorum assumenda nam obcaecati vitae in sequi!`,
+      duration: 330,
+      id: '2',
+      title: 'React Crash Course 2018',
+      topRated: true
+    },
+    {
+      creationDate: new Date(Date.now()),
+      description: `Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
+      est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
+      amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
+      laborum atque aut id necessitatibus asperiores minus debitis fugit amet
+      dolorum assumenda nam obcaecati vitae in sequi!`,
+      duration: 40,
+      id: '3',
+      title: 'Advanced Javascript',
+      topRated: true
+    },
+    {
+      creationDate: new Date(Date.now() + 86400000),
+      description: `Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
+      est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
+      amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
+      laborum atque aut id necessitatibus asperiores minus debitis fugit amet
+      dolorum assumenda nam obcaecati vitae in sequi!`,
+      duration: 50,
+      id: '4',
+      title: 'Nuxt.js Fundamentals',
+      topRated: false
+    },
+    {
+      creationDate: new Date(Date.now() + 172800000),
+      description: `Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
+      est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
+      amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
+      laborum atque aut id necessitatibus asperiores minus debitis fugit amet
+      dolorum assumenda nam obcaecati vitae in sequi!`,
+      duration: 90,
+      id: '5',
+      title: 'React Training: Advanced React.js',
+      topRated: false
+    }
+  ];
+
+  createCourse(newCourse: ICourse): void {
+    newCourse.id = Guid.raw();
+    CoursesService.courses.push(newCourse);
+  }
+
   getList(): ICourse[] {
-    const listOfCourses: ICourse[] = [
-      {
-        creationDate: new Date(),
-        description: `Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
-        est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
-        amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
-        laborum atque aut id necessitatibus asperiores minus debitis fugit amet
-        dolorum assumenda nam obcaecati vitae in sequi!`,
-        duration: 120,
-        id: '1',
-        title: 'JavaScript: Understanding the Weird Parts',
-        topRated: false
-      },
-      {
-        creationDate: new Date(),
-        description: `Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
-        est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
-        amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
-        laborum atque aut id necessitatibus asperiores minus debitis fugit amet
-        dolorum assumenda nam obcaecati vitae in sequi!`,
-        duration: 330,
-        id: '2',
-        title: 'React Crash Course 2018',
-        topRated: true
-      },
-      {
-        creationDate: new Date(),
-        description: `Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
-        est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
-        amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
-        laborum atque aut id necessitatibus asperiores minus debitis fugit amet
-        dolorum assumenda nam obcaecati vitae in sequi!`,
-        duration: 40,
-        id: '3',
-        title: 'Advanced Javascript',
-        topRated: true
-      },
-      {
-        creationDate: new Date(),
-        description: `Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
-        est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
-        amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
-        laborum atque aut id necessitatibus asperiores minus debitis fugit amet
-        dolorum assumenda nam obcaecati vitae in sequi!`,
-        duration: 50,
-        id: '4',
-        title: 'Nuxt.js Fundamentals',
-        topRated: false
-      },
-      {
-        creationDate: new Date(),
-        description: `Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ducimus, magnam! Distinctio, incidunt rem, accusantium
-        est ea totam nihil dolores tenetur excepturi expedita quod unde dolore
-        amet, eos voluptates similique quam. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Omnis, repudiandae impedit repellendus quos
-        laborum atque aut id necessitatibus asperiores minus debitis fugit amet
-        dolorum assumenda nam obcaecati vitae in sequi!`,
-        duration: 90,
-        id: '5',
-        title: 'React Training: Advanced React.js',
-        topRated: false
-      }
-    ];
-
-    listOfCourses.forEach(
-      (course: ICourse, index: number): void => {
-        course.creationDate.setDate(course.creationDate.getDate() + index - 2);
-      }
-    );
-
-    return listOfCourses;
+    return CoursesService.courses;
   }
 
   constructor() {}
