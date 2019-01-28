1191ef318b4b1c30a41749fd589fc457eb02ac80
Ihar Saroka <Ihar_Saroka@epam.com>
2019-01-28 13:01:30 +0300

    task-5-modules-and-services: created method IsAuthenticated for authorization service

--- src/app/core/services/authorization/authorization.service.ts
+++ src/app/core/services/authorization/authorization.service.ts
@@ -5,14 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class AuthorizationService {
 
-  login(email: string, password: string) {
-    localStorage.setItem('user-name', 'Fake User');
+  login(email: string, password: string): void {
+    localStorage.setItem('user-login', 'Fake User');
     localStorage.setItem('user-token', 'fake token');
   }
 
-  logout() {
-    localStorage.removeItem('user-name');
+  logout(): void {
+    localStorage.removeItem('user-login');
     localStorage.removeItem('user-token');
+  }
+
+  IsAuthenticated(): boolean {
+    return localStorage.getItem('user-login') !== null && localStorage.getItem('user-token') !== null;
   }
 
   constructor() { }
