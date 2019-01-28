c632955989181155cebf1e1b7a5b7db84c8ff4c4
Ihar Saroka <Ihar_Saroka@epam.com>
2019-01-28 14:20:13 +0300

    task-5-modules-and-services: added usage of AuthorizationService in user-login component

--- src/app/core/header/components/user-login/user-login.component.ts
+++ src/app/core/header/components/user-login/user-login.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizationService } from 'src/app/core/services';
 
 @Component({
   selector: 'learn-portal-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authorization: AuthorizationService) { }
 
+  userLogin: string;
+
+  ngOnInit() {
+    this.userLogin = this.authorization.getUserInfo();
+  }
 }
