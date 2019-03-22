99c205760685a24d536f246eb44c49358838a710
Ihar Saroka <Ihar_Saroka@epam.com>
2019-03-22 16:00:20 +0300

    task-9-rxjs: added api call on each search input event, using observable for input event of search element

--- src/app/courses/components/course-list/components/search-course/search-course.component.ts
+++ src/app/courses/components/course-list/components/search-course/search-course.component.ts
@@ -1,18 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+
+import {fromEvent, Subscription} from 'rxjs';
 
 @Component({
   selector: 'learn-portal-search-course',
   templateUrl: './search-course.component.html',
   styleUrls: ['./search-course.component.scss']
 })
-export class SearchCourseComponent {
+export class SearchCourseComponent implements OnInit, OnDestroy {
   constructor() {}
 
   @Output() searchActivated = new EventEmitter<string>();
 
+  @ViewChild('searchInput') searchInputRef: ElementRef;
   inputSearchValue = '';
+  searchInputSubscriber: Subscription;
 
   activateSearch() {
     this.searchActivated.emit(this.inputSearchValue);
+  }
+
+  ngOnInit() {
+    const searchInputEventObservable = fromEvent(this.searchInputRef.nativeElement, 'input');
+
+    this.searchInputSubscriber = searchInputEventObservable.subscribe(e => this.activateSearch());
+  }
+
+  ngOnDestroy() {
+    this.searchInputSubscriber.unsubscribe();
   }
 }
