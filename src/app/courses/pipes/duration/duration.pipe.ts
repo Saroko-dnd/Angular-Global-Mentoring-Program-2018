38a71b8f4059b32d2417c2ab4042f4a645ac97ce
saroko-dnd <isaroko@mail.ru>
2019-05-11 02:02:26 +0300

    task-12-internationalization: created russian localization for courses module

--- src/app/courses/pipes/duration/duration.pipe.ts
+++ src/app/courses/pipes/duration/duration.pipe.ts
@@ -1,16 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Pipe({
   name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
-  transform(duration: number): string {
-    if (duration === null) {
-      return '';
-    } else if (duration >= 60) {
-      return `${Math.trunc(duration / 60)}h ${duration % 60}min`;
-    } else {
-      return `${duration}min`;
-    }
+  constructor(private translate: TranslateService) {}
+
+  transform(duration: number): Observable<string> {
+    return this.translate
+      .stream(['COURSE_DURATION_HOURS', 'COURSE_DURATION_MINUTES'])
+      .pipe(
+        map(translations => {
+          if (duration === null) {
+            return '';
+          } else if (duration >= 60) {
+            return `${Math.trunc(duration / 60)}${
+              translations.COURSE_DURATION_HOURS
+            } ${duration % 60}${translations.COURSE_DURATION_MINUTES}`;
+          } else {
+            return `${duration}${translations.COURSE_DURATION_MINUTES}`;
+          }
+        })
+      );
   }
 }
